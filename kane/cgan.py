88372fd7929a0f5b916f760afed26e8035d08c7a
Kane Tian <kxt3@cornell.edu>
2020-05-17 16:53:36 -0400

    before i ruin things

--- kane/cgan.py
+++ kane/cgan.py
@@ -18,17 +18,25 @@ import pandas as pd
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+
+parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--n_classes", type=int, default=2, help="number of classes for dataset")
 parser.add_argument("--sample_interval", type=int, default=400, help="interval between drug sampling")
 parser.add_argument("--save_dir", type=str, default="generated/trial_1/", help="directory for saved models, generated examples")
 parser.add_argument("--save_interval", type=int, default=100, help="interval between model saving")
 opt = parser.parse_args()
 print(opt)
+
+def train():
+    pass
+
+def sample():
+    pass
 
 os.makedirs(opt.save_dir, exist_ok=True)
 gen_shape = (1, 51)
